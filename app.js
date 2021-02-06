836e1c6c37c443489a87dd1a604bc5b3f5b9d481
Delwar Ahmed <delwar.habib@gmail.com>
2021-02-06 21:19:19 +0600

    updated some script

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const apiKey = 'bf6469563a82df2403ea5a0473338f88';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', function () {
@@ -7,21 +8,17 @@ submitButton.addEventListener('click', function () {
     callData(cityInput);
 });
 
-function callData(cityInput) {
-    // const city = cityInput;
-    // const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`)
-    .then(res => res.json())
-    .then(data => displayData(data));
+function callData(city) {
+    const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data));
 }
 
-
 function displayData(data) {
-    console.log(data);
     document.getElementById('city-name').innerText = data.name || "Unknown Location";
     document.getElementById('temp').innerText = data.main.temp;
     document.getElementById('lead').innerText = data.weather[0].main;
     document.getElementById('icon').setAttribute('src', 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png');
-    cityInput = "";
-
+    document.getElementById('city-input').value = "";
 }
